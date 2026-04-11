b95ab9209ed560493a5f20a8c94b1eadd24eaac3
JianoFreo <jianofreo.magtangob@tup.edu.ph>
2026-04-12 07:15:08 +0800

    JWT auth

--- config/env.js
+++ config/env.js
@@ -1,11 +1,16 @@
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 /* This line of code is using the `config` function from the `dotenv` package to load environment
 variables from a specific `.env` file based on the current `NODE_ENV` environment variable. */
-config({path:`.env.${process.env.NODE_ENV || 'development'}.local`}); 
+config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 // this path is for local and and development
 
 
-export const {PORT, NODE_ENV, DB_URI} = process.env; // switch between dev and prod without overriding one another
+export const {
+    PORT,
+    NODE_ENV, DB_URI,
+    JWT_SECRET,
+    JWT_EXPIRES_IN
+} = process.env; // switch between dev and prod without overriding one another
 
 
 
