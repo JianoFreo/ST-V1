31e8b074cc2050aff91f7379c3e43756557dcf5a
JianoFreo <jianofreo.magtangob@tup.edu.ph>
2026-04-14 00:03:38 +0800

    authenticaton using JWT token and bearer token. signing in and grabbing the token and GET request on users with that token on the auth HTTTP

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,6 @@
 import  { Router } from 'express'
 import { getUser, getUsers} from '../controllers/user.controller.js'
+import authorize from '../middlewares/auth.middleware.js';
 
 const userRouter = Router();
 
@@ -12,7 +13,7 @@ const userRouter = Router();
 
 userRouter.get('/', getUsers);
 
-userRouter.get('/:id', getUser);
+userRouter.get('/:id', authorize, getUser);
 
 userRouter.post('/', (req, res) => res.send({title: 'CREATE new user'}));
 
