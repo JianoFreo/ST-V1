e3af84034c3b35ec62c5bd9ad197d3e26dee722f
JianoFreo <jianofreo.magtangob@tup.edu.ph>
2026-04-17 23:41:49 +0800

    working now

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 
 import { EMAIL_PASSWORD, EMAIL_USER } from './env.js'
 
-export const accountEmail = 'javascriptmastery00@gmail.com';
+export const accountEmail = 'jianofreomagtangob@gmail.com';
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
